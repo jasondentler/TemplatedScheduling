446e0eaeea62d621430a29e5c61be70e67d0c3ea
Jason Dentler <jasondentler@gmail.com>
2011-06-08 07:59:23 -0500

    Let me Resharper that for you.

--- src/ISIS.Web.Core/TemplateActionLinkExtensions.cs
+++ src/ISIS.Web.Core/TemplateActionLinkExtensions.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
 using System.Linq;
-using System.Text;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
